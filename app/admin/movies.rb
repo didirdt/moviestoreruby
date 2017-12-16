6b4121e29525bbe70d48c19ffb39ed89f4a824fa
didi <didi@41studio.com>
2017-12-16 15:19:34 +0700

    v1.2-fix admin

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Movie do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :title, :release_year, :price, :description, :imdb_id, :poster_url
 #
 # or
 #
@@ -12,4 +12,26 @@ ActiveAdmin.register Movie do
 #   permitted
 # end
 
+index do
+  column :title do |movie|
+    link_to movie.title, movie_path(movie)
+  end
+ 
+  actions
+end
+
+form do |f|
+    f.inputs "Admin Details" do
+      f.input :title
+      f.input :release_year
+      f.input :price
+      f.input :description
+      f.input :imdb_id
+      f.input :poster_url
+    end
+    f.button "Update"
+  end
+ 
+# Filter only by title
+filter :title
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :email, :password, :salt, :encrypted_password
 #
 # or
 #
@@ -11,5 +11,6 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
 
 end
