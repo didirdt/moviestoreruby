b9ea87db29f285aa3f9b2a8ebfeac10937e0ce2d
didi <didi@41studio.com>
2017-12-16 11:54:21 +0700

    v.1.1

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
+$(window).load ->
+  $('a[data-target]').click (e) ->
+    e.preventDefault()
+    $this = $(this)
+    if $this.data('target') == 'Add to'
+      url = $this.data('addurl')
+      new_target = "Remove from"
+    else
+      url = $this.data('removeurl')
+      new_target = "Add to"
+    $.ajax url: url, type: 'put', success: (data) ->
+      $('.cart-count').html(data)
+      $this.find('span').html(new_target)
+      $this.data('target', new_target)
+$(window).load ->
+  $('#mycart .fi-x').click (e) ->
+    e.preventDefault()
+    $this = $(this).closest('a')
+    url = $this.data('targeturl')
+    $.ajax url: url, type: 'put', success: (data) ->
+      $('.cart-count').html(data)
+      $this.closest('.cart-movie').slideUp()      
---
+++ app/models/movie.rb
@@ -0,0 +1,12 @@
+class Movie < ActiveRecord::Base
+  def poster
+    "http://ia.media-imdb.com/images/M/#{poster_url}"
+  end
+
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+  def cart_count
+  $redis.scard "cart#{id}"
+end
+end
